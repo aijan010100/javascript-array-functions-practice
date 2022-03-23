dd23f96113f2d5f713ee803949854681f1b83871
manatkyzya <55605662+aijan010100@users.noreply.github.com>
2022-03-23 15:10:10 -0700

    practicing map and reduce

--- worksheet.js
+++ worksheet.js
@@ -31,15 +31,55 @@ const characters = [
 
 //***MAP***
 //1. Get array of all names
-//2. Get array of all heights
-//3. Get array of objects with just name and height properties
-//4. Get array of all first names
 
-//***REDUCE***
+const task1 = characters.map((it => it.name))
+console.log(task1)
+
+//2. Get array of all heights
+
+const task2 = characters.map((it => it.height))
+console.log(task2)
+
+//3. Get array of objects with just name and height properties
+
+const task3 = characters.map((it => (`${it.name} ${it.height}`)))
+console.log(task3)
+
+// 4. Get array of all first names
+
+const task4 = characters.map((it) => {
+    [first, ...rest] = it.name.split(' ')
+    return first
+})
+console.log(task4)
+
+// ***REDUCE***
 //1. Get total mass of all characters
+
+// const totalMass = characters.reduce((acc, rec) => acc + rec.mass, 0)
+// console.log(totalMass)
+
 //2. Get total height of all characters
+
+// const totalHeight = characters.reduce((acc, rec) => acc + rec.height, 0)
+// console.log(totalHeight)
+
 //3. Get total number of characters by eye color
+const totalChars = characters.reduce((acc, rec) => {
+    const color = rec.eye_color
+    if(acc[color]) {
+        acc[color]++
+    }else {
+        acc[color] = 1
+    }
+    return acc
+} ,{})
+// console.log(totalChars)
+
 //4. Get total number of characters in all the character names
+
+const totalNameChars = characters.reduce((acc, rec) => acc + rec.name, 0)
+console.log(totalNameChars)
 
 //***FILTER***
 //1. Get characters with mass greater than 100
