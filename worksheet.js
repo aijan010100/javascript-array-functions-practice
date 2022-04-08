6c4822f58684ffa31ca9a6b55aa9865442d0e972
Aizhan Manatkyzy <aizhanmanatkyzy@gmail.com>
2022-04-08 16:51:43 -0700

    updates

--- worksheet.js
+++ worksheet.js
@@ -30,56 +30,63 @@ const characters = [
 ];
 
 //***MAP***
-//1. Get array of all names
+// //1. Get array of all names
 
-const task1 = characters.map((it => it.name))
-console.log(task1)
+// const task1 = characters.map((it => it.name))
+// console.log(task1)
 
-//2. Get array of all heights
+// //2. Get array of all heights
 
-const task2 = characters.map((it => it.height))
-console.log(task2)
+// const task2 = characters.map((it => it.height))
+// console.log(task2)
 
-//3. Get array of objects with just name and height properties
+// //3. Get array of objects with just name and height properties
 
-const task3 = characters.map((it => (`${it.name} ${it.height}`)))
-console.log(task3)
+// const task3 = characters.map((it => (`${it.name} ${it.height}`)))
+// console.log(task3)
 
-// 4. Get array of all first names
+// // 4. Get array of all first names
 
-const task4 = characters.map((it) => {
-    [first, ...rest] = it.name.split(' ')
-    return first
-})
-console.log(task4)
+// const task4 = characters.map((it) => {
+//     [first, ...rest] = it.name.split(' ')
+//     return first
+// })
+// console.log(task4)
 
-// ***REDUCE***
-//1. Get total mass of all characters
+// // ***REDUCE***
+// // 1. Get total mass of all characters
 
 // const totalMass = characters.reduce((acc, rec) => acc + rec.mass, 0)
 // console.log(totalMass)
 
-//2. Get total height of all characters
+// // 2. Get total height of all characters
 
 // const totalHeight = characters.reduce((acc, rec) => acc + rec.height, 0)
 // console.log(totalHeight)
 
-//3. Get total number of characters by eye color
-const totalChars = characters.reduce((acc, rec) => {
-    const color = rec.eye_color
-    if(acc[color]) {
-        acc[color]++
-    }else {
-        acc[color] = 1
-    }
-    return acc
-} ,{})
-// console.log(totalChars)
+// 3. Get total number of characters by eye color
+// const totalChars2 = characters.reduce((acc, rec) => {
+//     const color = rec.eye_color
+//     if(acc[color]) {
+//         acc[color]++
+//     } else {
+//         acc[color] = 1
+//     }
+//     return acc
+// } ,{})
+
+    const totalChars = characters.reduce((acc, rec) => {
+        const color = rec.eye_color
+        return acc[color] ? acc[color] + 1 : acc[color]
+    }, {})
+console.log(totalChars)
+// console.log(totalChars2)
+
 
 //4. Get total number of characters in all the character names
 
-const totalNameChars = characters.reduce((acc, rec) => acc + rec.name, 0)
-console.log(totalNameChars)
+// const totalNameChars = characters.reduce((acc, rec) => acc + rec.name.length, 0)
+// console.log(totalNameChars)
 
 //***FILTER***
 //1. Get characters with mass greater than 100
@@ -104,3 +111,5 @@ console.log(totalNameChars)
 //2. Is there at least one character with blue eyes?
 //3. Is there at least one character taller than 210?
 //4. Is there at least one character that has mass less than 50?
+
+
